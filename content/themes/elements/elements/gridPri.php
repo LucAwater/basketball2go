428c0e5f73d51239af945c7f8138dcf8281685b5
Luc Awater <lucawater@gmail.com>
2016-04-27 09:41:22 +0200

    Remove deprecated options from elements

--- content/themes/elements/elements/gridPri.php
+++ content/themes/elements/elements/gridPri.php
@@ -1,32 +1,19 @@
 <?php
-// Options (variables)
-$o_menu = get_sub_field( 'gridPri_o_menu' );
-
-$o_h_pos = get_sub_field( 'gridPri_o_h_pos' );
-$o_h_align = get_sub_field( 'gridPri_o_h_align' );
-$o_b_layout = get_sub_field( 'gridPri_o_b_layout' );
-
 // Content (variables)
 $h_title = get_sub_field( 'gridPri_h_title' );
-$h_text = preg_replace( '/<p>/', '<p class="is-aligned-' . $o_h_align . '">', get_sub_field( 'gridPri_h_text' ) );
+$h_text = wpautop( get_sub_field( 'gridPri_h_text' ) );
 $b_images = get_sub_field( 'gridPri_b_images' );
 
 // Classes
 $class_section = 'grid grid-pri';
-$class_header = 'section-header is-pos-' . $o_h_pos;
+$class_header = 'section-header';
 $class_body = 'section-body';
-
-if( $o_b_layout == 'masonry' ){
-  $class_grid = 's-grid-1 m-grid-2 l-grid-4 row isotope isotope-masonry';
-} else {
-  $class_grid = 's-grid-1 m-grid-2 l-grid-4 row';
-}
 
 // Build section
 section_start( $class_section );
 
   // Header
-  section_header($class_header, $h_title, $h_text, $o_h_align);
+  section_header($class_header, $h_title, $h_text);
 
   // Body
   section_body_start( $class_body );
