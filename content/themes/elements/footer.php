56bf18b4aae7d32d4a1b152cfed3b4f064bd0d10
Luc Awater <lucawater@gmail.com>
2016-05-22 16:53:46 +0200

    Add footer styling

--- content/themes/elements/footer.php
+++ content/themes/elements/footer.php
@@ -1,5 +1,28 @@
   </main>
 
+  <footer>
+    <img src="<?php echo bloginfo( 'template_directory' ); ?>/img/logo-black.svg">
+
+    <nav>
+      <ul>
+        <?php
+        $nav = array(
+          'theme_location'  => 'menu_primary',
+          'container'       => '',
+          'items_wrap'      => '%3$s',
+          'depth'           => 1
+        );
+
+        wp_nav_menu( $nav );
+        ?>
+
+        <li><a>Contact</a></li>
+      </ul>
+    </nav>
+
+    <small>&copy; 2016 - CAB & CAB Kids</small>
+  </footer>
+
   <!-- Scripts -->
   <?php wp_footer(); ?>
 </body>
