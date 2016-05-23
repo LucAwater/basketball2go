065c7e35f969e502d3c7e8546edebfa3e441563b
Luc Awater <lucawater@gmail.com>
2016-05-23 12:37:02 +0200

    Remove unnecessary elements from post-thumb

--- content/themes/elements/content-post.php
+++ content/themes/elements/content-post.php
@@ -10,14 +10,7 @@ $thumb = get_the_post_thumbnail( $post->ID, 'medium' );
 ?>
 
 <li>
-  <div>
-    <a href="<?php echo $category_link; ?>" class="post-category"><?php echo $category[0]->cat_name; ?></a>
-    <a class="post-title" href="<?php echo $permalink; ?>"><h3><?php echo $title; ?></h3></a>
-  </div>
+  <a class="post-title" href="<?php echo $permalink; ?>"><h3><?php echo $title; ?></h3></a>
 
   <?php echo $thumb; ?>
-
-  <div>
-    <p><?php echo $date; ?></p>
-  </div>
 </li>
