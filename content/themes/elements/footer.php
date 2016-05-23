53205bd8a6c2e8712ff8e0324093cd65dfee4877
Luc Awater <lucawater@gmail.com>
2016-05-23 12:15:19 +0200

    Temporarily reduced footer content

--- content/themes/elements/footer.php
+++ content/themes/elements/footer.php
@@ -1,24 +1,7 @@
   </main>
 
   <footer>
-    <img src="<?php echo bloginfo( 'template_directory' ); ?>/img/logo-black.svg">
-
-    <nav>
-      <ul>
-        <?php
-        $nav = array(
-          'theme_location'  => 'menu_primary',
-          'container'       => '',
-          'items_wrap'      => '%3$s',
-          'depth'           => 1
-        );
-
-        wp_nav_menu( $nav );
-        ?>
-
-        <li><a>Contact</a></li>
-      </ul>
-    </nav>
+    
 
     <small>&copy; 2016 - CAB & CAB Kids</small>
   </footer>
